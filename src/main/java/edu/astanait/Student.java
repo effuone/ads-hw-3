842d12dc566ac17775733c2d5ef7746d4fc2f69e
effuone <albkfil@gmail.com>
2024-05-20 08:37:28 +0500

    student class added

---
+++ src/main/java/edu/astanait/Student.java
@@ -0,0 +1,16 @@
+package edu.astanait;
+
+public class Student {
+    private final String name;
+    private final int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + age + ")";
+    }
+}
